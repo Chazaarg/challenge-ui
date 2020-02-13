3f7d3454d0f5e1507065b183dfcc534aa3794dbd
Patricio Chazarreta <chazarreta.patricio@gmail.com>
2020-02-13 12:42:02 -0300

    Updates

---
+++ precache-manifest.efbd510c9f81e9049bf5eeccc198ff76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde9690a46efd80e2d9569fa031f456a",
+    "url": "/challenge-ui/index.html"
+  },
+  {
+    "revision": "7ccd83fcb04966337746",
+    "url": "/challenge-ui/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8b7522b21e51ef2d6d50",
+    "url": "/challenge-ui/static/js/2.d0466087.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/challenge-ui/static/js/2.d0466087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccd83fcb04966337746",
+    "url": "/challenge-ui/static/js/main.8faf7e2c.chunk.js"
+  },
+  {
+    "revision": "ce0ac279a57efa98f74b",
+    "url": "/challenge-ui/static/js/runtime-main.0e8e3c72.js"
+  }
+]);
